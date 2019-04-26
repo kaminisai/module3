139fe1cf39a26572135d1d93a327e8a1fc7a1664
kaminisai <44118675+kaminisai@users.noreply.github.com>
2019-04-26 13:45:43 +0530

    Add files via upload

---
+++ SpringBootTDSApplication/src/main/java/com/example/demo/SpringBootTdsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTdsApplication.class, args);
+	}
+
+}
